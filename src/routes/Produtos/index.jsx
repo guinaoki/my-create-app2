27806fa393fd69a3e6ea3218fbb38da5db57264b
guinaoki <rm551456@fiap.com.br>
2023-09-05 17:30:14 -0300

    deixando mais bonitinho

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,29 +1,22 @@
 import { Link } from "react-router-dom"
 import { listaProdutos } from "../../JS/listaProdutos"
+import styles from './Produtos.css'
 import { BiSolidKeyboard } from 'react-icons/bi'
 import {BsFillMouse2Fill} from 'react-icons/bs'
 import {FiMonitor} from 'react-icons/fi'
+import {BiSolidPencil} from 'react-icons/bi'
 
 export default function Produtos(){
     return(
         <main>
             <h1>Produtos</h1>
             {listaProdutos.map(prod => (
-                <div key={prod.id}>
+                <div key={prod.id} className="style.nomeProdutos">
                     <Link to={`/produtos/editar/${prod.id}`}>
-                        Editar o produto: {prod.nome}
+                       <BiSolidPencil/> Editar o produto: {prod.nome} 
                     </Link>
                 </div>
             ))}
-            <div className="logoTeclado">
-                <BiSolidKeyboard size='1rem'/>
-            </div>
-            <div className="logoMouse">
-                <BsFillMouse2Fill size='1rem'/>
-            </div>
-            <div className="logoMonitor">
-                <FiMonitor size='1rem'/>
-            </div>
         </main>
     )
 }

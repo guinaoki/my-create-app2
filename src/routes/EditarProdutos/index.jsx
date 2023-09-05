0c67bf8bdae7f09671721665e9d1c22b06af4841
guinaoki <rm551456@fiap.com.br>
2023-09-05 16:55:44 -0300

    página de editar o produto feita

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,7 +1,26 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { listaProdutos } from "../../JS/listaProdutos"
+
 export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        alert(`Produto:  ${produto.nome} editado com sucesso!`)
+        return navegacao('/produtos')
+    }
+
     return(
         <main>
             <h1>Editando o Produto</h1>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick={salvar}>Salvar</button>
         </main>
     )
 }
